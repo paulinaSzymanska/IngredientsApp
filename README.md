9aba499c23e4b73ab57da68ccb62475f6fae4dc8
Paulina Szymanska <paulina.szymanska1997@interia.pl>
2024-02-26 21:58:04 +0000

    New files added: README.md, pdf files

---
+++ README.md
@@ -0,0 +1,13 @@
+## How to run app from terminal?
+
+- Set adb environment variable
+- Run emulator
+- adb push recipes/breakfast.pdf /sdcard/Download
+- adb push recipes/dinner.pdf /sdcard/Download
+- adb push recipes/supper.pdf /sdcard/Download
+- adb push recipes/dessert.pdf /sdcard/Download
+
+and the run app 
+
+## if you run app from phone
+just enjoy!
--- app/src/main/java/com/example/ingredientsapp/MainActivity.java
+++ app/src/main/java/com/example/ingredientsapp/MainActivity.java
@@ -15,9 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button load_recipes = findViewById(R.id.load_recipes);
+        Button chooseBreakFastFile = findViewById(R.id.breakfastButton);
 
         load_recipes.setOnClickListener(view -> {
             Intent intent = new Intent(this, Breakfast.class);
+            startActivity(intent);
+        });
+
+        chooseBreakFastFile.setOnClickListener(view -> {
+            //  android.content.ActivityNotFoundException: No Activity found to handle Intent {
+            //  act=android.intent.action.GET_CONTENT }
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             startActivity(intent);
         });
 
