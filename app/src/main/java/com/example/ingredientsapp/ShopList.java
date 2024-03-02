3d1a5bd200c5d705ed8eeec944c11c4add48b6d4
Paulina Szymanska <paulina.szymanska1997@interia.pl>
2024-03-02 16:48:15 +0000

    First step to add generating pdf with shopping list

--- app/src/main/java/com/example/ingredientsapp/ShopList.java
+++ app/src/main/java/com/example/ingredientsapp/ShopList.java
@@ -1,15 +1,27 @@
 package com.example.ingredientsapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Comparator;
+
 
 public class ShopList extends AppCompatActivity {
     private StringBuilder sb = new StringBuilder();
@@ -19,10 +31,13 @@ public class ShopList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_list);
         TextView shopList = findViewById(R.id.list);
+        Button saveShopListButton = findViewById(R.id.saveShopList);
 
         File mainDirectory = new File("/data/user/0/com.example.ingredientsapp/cache/");
-
         File[] txtDirectories = mainDirectory.listFiles((dir, name) -> name.startsWith("txt") && new File(dir, name).isDirectory());
+
+        saveShopListButton.setOnClickListener(view -> generatePdf(this, sb));
+
         readSavedTxtFiles(txtDirectories, shopList);
     }
 
@@ -71,5 +86,34 @@ public class ShopList extends AppCompatActivity {
             }
             return sortedString.toString();
         } else return "";
+    }
+
+
+    private void generatePdf(Context context, StringBuilder string) {
+        PdfDocument pdf = null;
+        try {
+            File pdfFile = new File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "shopList.pdf");
+            PdfWriter writer = new PdfWriter(pdfFile);
+            pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            document.add(new Paragraph(string.toString()));
+
+            System.out.println("Plik PDF został utworzony pomyślnie.");
+            openPdf(context, Uri.fromFile(pdfFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (pdf != null) {
+                pdf.close();
+            }
+        }
+    }
+
+    private void openPdf(Context context, Uri uri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "application/pdf");
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        context.startActivity(intent);
     }
 }
